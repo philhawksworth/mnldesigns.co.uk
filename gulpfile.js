9e8193df5e6cabc375dfc97439d453864b5ac974
Phil Hawksworth <phawksworth@gmail.com>
2014-07-08 20:18:16 +0100

    test build

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var connect = require('gulp-connect');
 gulp.task('build', function() {
   // Usually we would minify and concatenate, but on this project
   // we'll just throw everything into the dist folder
-  return gulp.src(["_site/*", "CNAME"])
+  return gulp.src(["_site/**/*", "CNAME"])
     .pipe(gulp.dest('dist'));
 });
 
