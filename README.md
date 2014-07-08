184a34f3a1204189c87a57f1f343fa5b51cca6e8
Phil Hawksworth <phawksworth@gmail.com>
2014-07-08 19:52:49 +0100

    build mechanics and simplified skin

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ mnldesigns.co.uk
 ===============
 
 Home page for MNL Designs surveying and building services http://mnldesigns.co.uk
+
+
---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+var connect = require('gulp-connect');
+
+gulp.task('build', function() {
+  // Usually we would minify and concatenate, but on this project
+  // we'll just throw everything into the dist folder
+  return gulp.src(["_site/*", "CNAME"])
+    .pipe(gulp.dest('dist'));
+});
+
+// run a local server
+gulp.task('serve', function() {
+  connect.server({
+	root: 'dist',
+	port: 8000,
+  });
+});
+
+
+// deploy by pushing the dist subtree to gh-pages
+gulp.task('deploy', ['build'], shell.task([
+  'git subtree push --prefix dist origin gh-pages'
+]));
+
+// The default task builds locally, and serves it up.
+gulp.task('default', ['build', 'serve']);
+
+
